05538e2d43562fc0010a0ccfb270870a3e88cf40
Isola Pelumi <pelumiisola87@gmail.com>
2024-11-24 00:06:43 +0100

    feat: table added

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -1,15 +1,33 @@
+import ChartOne from "@/components/dashboard/home/ChartOne";
+import { BarChartData } from "@/constants/data";
 import DashboardLayout from "@/layouts/dasboard";
+import Tables from "../tables/page";
+import Table from "@/components/dashboard/home/Table";
 
 export default function Dashboard() {
   return (
     <DashboardLayout pageTitle="Dashboard">
-      <div className="w-full ml-60 px-5 bg-[#F9F9FA] min-h-screen">
+      <div className="w-full pl-[260px] px-5 bg-[#F9F9FA] min-h-screen overflow-x-scroll pr-5">
         <h2 className="font-semibold text-2xl text-gray-800">
           Hello, Isola Pelumi 👋
         </h2>
         <p className="text-sm text-gray-600 py-2">
           Here is what’s happening on Dashboard Since it was launced
         </p>
+
+        <section className="mt-20">
+          <div className="bg-white rounded-xl p-5 w-full flex flex-col gap-5">
+            <h1 className="text-gray-700">Bar Chart</h1>
+            <ChartOne chartData={BarChartData.chartData} />
+          </div>
+
+          <div className="bg-white mt-8 rounded-xl relative py-5 w-full flex flex-col gap-5 mb-10">
+           <div className="sticky top-0">
+           <h1 className="text-gray-700 px-5">Table</h1>
+           </div>
+            <Table />
+          </div>
+        </section>
       </div>
     </DashboardLayout>
   );
